4efe8f3a88188246c3bfb267cae97a32cb18b662
AntiElitz <berghoffacc@gmail.com>
2025-04-23 04:25:06 +0200

    updated links

--- viewer/viewer_config.js
+++ viewer/viewer_config.js
@@ -1,10 +1,10 @@
 const viewerConfig = {
   planetPreviewSources: {
     nauvis: "https://dl.dropboxusercontent.com/scl/fi/huherqqyj03sigzgxsy3j/nauvis.png?rlkey=jdokw6ikwp5gwtw0gvch12lyx",
-    vulcanus: "https://dl.dropboxusercontent.com/scl/fi/rsrlwpx74o7fid0w120pl/vulcanus.png?rlkey=xcn2b9c9wmbgz0fpxeh3n3lsm",
-    gleba: "https://dl.dropboxusercontent.com/scl/fi/2toga8hoj5imzral6089p/gleba.png?rlkey=j0yoam2q31m3t9ivqwl0nyc0m",
-    fulgora: "https://dl.dropboxusercontent.com/scl/fi/ovwa1wih63bdub1tl648m/fulgora.png?rlkey=3i45j41h2lp0y682kh3p62x1t",
-    aquilo: "https://dl.dropboxusercontent.com/scl/fi/obqa00xhamo5q498e4sgl/aquilo.png?rlkey=g1rperlbmx3wmfhbdmx9wx6mt",
+    vulcanus: "https://dl.dropboxusercontent.com/scl/fi/cd5b8bz1zoekn1tddi76r/vulcanus.png?rlkey=hsmm3cxsx7qrkk25832r9se8x",
+    gleba: "https://dl.dropboxusercontent.com/scl/fi/mi8h28agttvwzq8zf9prk/gleba.png?rlkey=w2de1eyj9y29k0qss5hey79fc",
+    fulgora: "https://dl.dropboxusercontent.com/scl/fi/kbagr4k2df0u8km4hhe2a/fulgora.png?rlkey=e9lhj86dferlw1xxwcnat1qp1",
+    aquilo: "https://dl.dropboxusercontent.com/scl/fi/70lebvib93nejmvz1xqmz/aquilo.png?rlkey=3bnxy1pmf1y7clttrlhdckhjp",
   },
-  planetNamesSource: "https://dl.dropboxusercontent.com/scl/fi/2tj5j7p2z1ya5or1ecn7h/remote_planet_names.json?rlkey=uq7f59qd46ms86xctcbgehjwv"
+  planetNamesSource: "https://dl.dropboxusercontent.com/scl/fi/6pz1zbjbghkbp73w39xon/remote_planet_names.json?rlkey=ouzuwpothslzzg7wqwwxa5pqq"
 };
